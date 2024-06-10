266c9b8a86e4e54d2e2ebb70715099fa305025e1
Siddharaj Yadav <sidharajyadav157@gmail.com>
2024-06-10 19:50:16 +0530

    code changes

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-        TODO:  <Outlet />
+        Blog:  <Outlet />
         </main>
         <Footer />
       </div>
